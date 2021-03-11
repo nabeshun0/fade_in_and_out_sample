93b5b761884a9ccb8843723658d2a9b201c4b33d
snt-nbzw <sntnbzw@gmail.com>
2021-03-11 11:03:33 +0900

    Add: implement fade in and out

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,56 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  // runApp(MyApp());
+  final appTitle = 'Opacoty Demo';
+  runApp(MaterialApp(
+    title: appTitle,
+    home: MyHomePage(
+      title: appTitle,
+    ),
+  ));
+}
+
+class MyHomePage extends StatefulWidget {
+  final String title;
+
+  MyHomePage({
+    Key? key,
+    required this.title,
+  });
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  bool _visible = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: AnimatedOpacity(
+          opacity: _visible ? 1 : 0,
+          duration: Duration(milliseconds: 500),
+          child: Container(
+            width: 200,
+            height: 200,
+            color: Colors.green,
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            _visible = !_visible;
+          });
+        },
+        tooltip: 'Toggle Opacity',
+        child: Icon(Icons.flip),
+      ),
+    );
+  }
 }
